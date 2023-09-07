3d35e81bcfb2b42ca6c50ac4083143f294feceda
Mohamed Ahmed <i.mohamedahhmed@gmail.com>
2023-09-07 20:09:19 +0300

    create a new project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:booklyy/features/splash_features/presentasion_layer/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -12,8 +13,8 @@ class Bookly extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      home :
+    return const GetMaterialApp(
+      home : SplashView(),
     );
   }
 }
