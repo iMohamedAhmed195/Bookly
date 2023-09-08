4e5ccf05a0dbfd78a28dea21d3d69a1b7b192747
Mohamed Ahmed <i.mohamedahhmed@gmail.com>
2023-09-08 14:52:39 +0300

    create style file and add title to home view

--- lib/features/home_feature/presentation/view/widget/custom_list_view_book.dart
+++ lib/features/home_feature/presentation/view/widget/custom_list_view_book.dart
@@ -12,21 +12,3 @@ class CustomListItem extends StatelessWidget {
         child: SvgPicture.asset(AssetsData.book , ));
   }
 }
-class FeatureBooksListView extends StatelessWidget {
-  const FeatureBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              child: CustomListItem(),
-            );
-          }),
-    );
-  }
-}
--- lib/features/home_feature/presentation/view/widget/home_view_body.dart
+++ lib/features/home_feature/presentation/view/widget/home_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:booklyy/core/utils/styles.dart';
 import 'package:booklyy/features/home_feature/presentation/view/widget/custom_appbar.dart';
-import 'package:booklyy/features/home_feature/presentation/view/widget/custom_list_view_book.dart';
+
+import 'package:booklyy/features/home_feature/presentation/view/widget/featured_list_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -7,11 +9,21 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeatureBooksListView(),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeatureBooksListView(),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0 ,right: 20.0,top: 15),
+            child: Text(
+              'Best Seller',style: Styles.titleMedium,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
