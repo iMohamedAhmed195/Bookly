e15b684679d56fa46994a46bdea33340bf593444
Mohamed Ahmed <i.mohamedahhmed@gmail.com>
2023-09-10 20:04:16 +0300

    put data in item of listview for best seller

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 
 abstract class Styles {
   static const textStyle20 = TextStyle(
-      fontSize: 20, fontWeight: FontWeight.normal, fontFamily: kGtSectraFine);
+      fontSize: 20, fontWeight: FontWeight.normal, );
   static const textStyle30 = TextStyle(
       fontSize: 30, fontWeight: FontWeight.normal, fontFamily: kGtSectraFine);
   static const textStyle18 = TextStyle(
--- lib/features/home_feature/presentation/view/widget/home_view_body.dart
+++ lib/features/home_feature/presentation/view/widget/home_view_body.dart
@@ -1,10 +1,10 @@
-
+import 'package:booklyy/constants.dart';
 import 'package:booklyy/core/utils/assets.dart';
 import 'package:booklyy/core/utils/styles.dart';
+import 'package:booklyy/features/home_feature/presentation/view/widget/best_seller_list_item.dart';
 import 'package:booklyy/features/home_feature/presentation/view/widget/custom_appbar.dart';
 
 import 'package:booklyy/features/home_feature/presentation/view/widget/featured_listview_home.dart';
-
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -14,7 +14,7 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   const Padding(
+    return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 10.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -22,44 +22,17 @@ class HomeViewBody extends StatelessWidget {
           CustomAppBar(),
           FeatureBooksListView(),
           Padding(
-            padding: EdgeInsets.only(left: 10.0 ,right: 20.0,top: 10,bottom: 20),
-            child: Text('Best Seller',style: Styles.textStyle18,
+            padding:
+                EdgeInsets.only(left: 10.0, right: 20.0, top: 10, bottom: 20),
+            child: Text(
+              'Best Seller',
+              style: Styles.textStyle18,
             ),
           ),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.0),
             child: BestSellerListViewItem(),
           ),
-        ],
-      ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 120,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2/3,
-              child: SvgPicture.asset(AssetsData.book)),
-          const SizedBox(width: 30,),
-           Column(
-            children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: const Text(
-                      'Harry Potter and the goblet of Fire',
-                      style: Styles.textStyle20,maxLines: 2,overflow: TextOverflow.ellipsis,
-                    ),
-                  )
-            ],
-          )
         ],
       ),
     );
