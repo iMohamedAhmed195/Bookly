1081241d29e67c19adad34412acac7df5617f461
Mohamed Ahmed <i.mohamedahhmed@gmail.com>
2023-09-10 05:23:38 +0300

    this is shit

--- lib/features/home_feature/presentation/view/widget/home_view_body.dart
+++ lib/features/home_feature/presentation/view/widget/home_view_body.dart
@@ -1,7 +1,7 @@
-import 'package:booklyy/core/utils/styles.dart';
+
 import 'package:booklyy/features/home_feature/presentation/view/widget/custom_appbar.dart';
 
-import 'package:booklyy/features/home_feature/presentation/view/widget/featured_list_view.dart';
+
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -15,11 +15,11 @@ class HomeViewBody extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomAppBar(),
-          FeatureBooksListView(),
+          // FeatureBooksListView(),
           Padding(
             padding: EdgeInsets.only(left: 15.0 ,right: 20.0,top: 15),
-            child: Text(
-              'Best Seller',style: Styles.titleMedium,
+            child: Text('asd'
+              // 'Best Seller',style: Styles.titleMedium,
             ),
           )
         ],
