944e59a7d66f96c53923f4023d14dba50c37fb23
Mohamed Ahmed <i.mohamedahhmed@gmail.com>
2023-09-10 19:53:08 +0300

    created custom list view for best seller

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor =  Color(0xff100B20);
 const kTransitionAnimation = Duration(milliseconds: 250);
+const kGtSectraFine= 'GT Sectra Fine' ;
+const kMendalion= 'Mendalion' ;
--- lib/features/home_feature/presentation/view/widget/home_view_body.dart
+++ lib/features/home_feature/presentation/view/widget/home_view_body.dart
@@ -1,28 +1,64 @@
 
+import 'package:booklyy/core/utils/assets.dart';
 import 'package:booklyy/core/utils/styles.dart';
 import 'package:booklyy/features/home_feature/presentation/view/widget/custom_appbar.dart';
-import 'package:booklyy/features/home_feature/presentation/view/widget/custom_list_view_book.dart';
+
 import 'package:booklyy/features/home_feature/presentation/view/widget/featured_listview_home.dart';
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return   const Padding(
       padding: EdgeInsets.symmetric(horizontal: 10.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomAppBar(),
-           FeatureBooksListView(),
+          FeatureBooksListView(),
           Padding(
-            padding: EdgeInsets.only(left: 15.0 ,right: 20.0,top: 10),
-            child: Text('Best Seller',style: Styles.titleMedium,
+            padding: EdgeInsets.only(left: 10.0 ,right: 20.0,top: 10,bottom: 20),
+            child: Text('Best Seller',style: Styles.textStyle18,
             ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: BestSellerListViewItem(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2/3,
+              child: SvgPicture.asset(AssetsData.book)),
+          const SizedBox(width: 30,),
+           Column(
+            children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    child: const Text(
+                      'Harry Potter and the goblet of Fire',
+                      style: Styles.textStyle20,maxLines: 2,overflow: TextOverflow.ellipsis,
+                    ),
+                  )
+            ],
           )
         ],
       ),
