63d033f9e394e9e218627e4a5ddca12c74093de2
Mohamed Ahmed <i.mohamedahhmed@gmail.com>
2023-09-10 23:35:15 +0300

    add title to book details

--- lib/features/home_feature/presentation/view/widget/book_details_view_body.dart
+++ lib/features/home_feature/presentation/view/widget/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:booklyy/core/utils/styles.dart';
 import 'package:booklyy/features/home_feature/presentation/view/widget/custom_book_details_app_bar.dart';
 import 'package:booklyy/features/home_feature/presentation/view/widget/custom_book_item.dart';
 import 'package:flutter/material.dart';
@@ -16,7 +17,18 @@ class BookDetailsViewBody extends StatelessWidget {
           Padding(
             padding: EdgeInsets.symmetric(horizontal: width * 0.19),
             child: const CustomBookImage(),
-          )
+          ),
+          const SizedBox(height: 43,),
+           Text(
+            'The Jungle Book' , style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 6,),
+            Opacity(
+              opacity: 0.7,
+              child: Text(
+              'Rudyard  Kipling' , style: Styles.textStyle18.copyWith(fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+          ),
+            )
         ],
       ),
     );
